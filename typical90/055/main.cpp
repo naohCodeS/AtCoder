b945a963bb6b4f1d849322556bbc9c1ff2422066
naohCodeS <naoh.suzuki@icloud.com>
2022-10-06 23:31:51 +0900

    055,061

--- typical90/055/main.cpp
+++ typical90/055/main.cpp
@@ -1,17 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
-
 #define ll long long
-#define P pair<ll,ll>
-#define rep(i, n) for(ll i=0; i<(ll)(n); i++)
-
-ll n,p,q;
-vector<ll> a;
 
 int main(){
-    cin>>n>>p>>q;
-    a.resize(n);
-    rep(i,n) cin>>a[i];
-
+	ll n,p,q;
+  	ll ans = 0;
+  	vector<ll> a;
+  	cin>>n>>p>>q;
+  	a.resize(n);
+	for(ll i=0 ;i<n; i++) cin>>a[i];
+  	for(ll i=0; i<n-4; i++){
+    	for(ll j=i+1; j<n-3; j++){
+          	for(ll k=j+1; k<n-2; k++){
+            	for(ll l=k+1; l<n-1; l++){
+                	for(ll m=l+1; m<n; m++){
+                      	if(a[i]%p*a[j]%p*a[k]%p*a[l]%p*a[m]%p == q){
+                          	ans++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+  	cout<<ans<<endl;
 }
--- typical90/061/main.cpp
+++ typical90/061/main.cpp
@@ -6,6 +6,27 @@ using namespace std;
 #define P pair<ll,ll>
 #define rep(i, n) for(ll i=0; i<(ll)(n); i++)
 
+ll q;
+vector<ll> deckb, deckf;
+
 int main(){
-    
+    cin>>q;
+    rep(i,q){
+        ll t,x;
+        cin>>t>>x;
+        if(t==1){
+            deckb.push_back(x);
+        }
+        else if(t==2){
+            deckf.push_back(x);
+        }
+        else{
+            if(deckb.size() >= x){
+                cout<<deckb[deckb.size() - x]<<endl;
+            }
+            else{
+                cout<<deckf[x - deckb.size() - 1]<<endl;
+            }
+        }
+    }
 }

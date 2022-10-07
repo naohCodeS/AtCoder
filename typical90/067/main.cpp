54c8d3e8a20472ccebca28620e524d9e409af7c9
naohCodeS <naoh.suzuki@icloud.com>
2022-10-07 19:10:11 +0900

    067

--- typical90/067/main.cpp
+++ typical90/067/main.cpp
@@ -6,6 +6,50 @@ using namespace std;
 #define P pair<ll,ll>
 #define rep(i, n) for(ll i=0; i<(ll)(n); i++)
 
+ll n,k;
+string s;
+
+class Radix {
+private:
+  const char* s;
+  int a[128];
+public:
+  Radix(const char* s = "0123456789ABCDEF") : s(s) {
+    int i;
+    for(i = 0; s[i]; ++i)
+      a[(int)s[i]] = i;
+  }
+  std::string to(long long p, int q) {
+    int i;
+    if(!p)
+      return "0";
+    char t[64] = { };
+    for(i = 62; p; --i) {
+      t[i] = s[p % q];
+      p /= q;
+    }
+    return std::string(t + i + 1);
+  }
+  std::string to(const std::string& t, int p, int q) {
+    return to(to(t, p), q);
+  }
+  long long to(const std::string& t, int p) {
+    int i;
+    long long sm = a[(int)t[0]];
+    for(i = 1; i < (int)t.length(); ++i)
+      sm = sm * p + a[(int)t[i]];
+    return sm;
+  }
+};
+
 int main(){
-    
+    cin>>s>>k;
+    Radix r;
+    rep(i,k){
+        s = r.to(s, 8, 9);
+        rep(j, s.size()){
+            if(s[j] == '8') s[j] = '5';
+        }
+    }
+    cout<<s<<endl;
 }
